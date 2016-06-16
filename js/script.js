02fdd9f6f94d2120f3c9e6eddee032b756ec56bb
Ryan Walker <rwalker3801@gmail.com>
2016-06-16 17:18:23 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.getElementById("footer").innerHTML =
+"<p>&copy;  " + new Date().getFullYear() + " Ryan Walker</p>";
+document.getElementById("nav01").innerHTML =
+ "<ul id='menu'>" +
+"<li><a href='index.html'>Home</a></li>" +
+"<li><a href='about-me.html'>About Me</a></li>" +
+"<li><a href='favorite-links.html'>Favorite Links</a></li>" +
+ "</ul>"; 
