491a3bbb4a95b1794dc19e6e3bafb72a81c8e17a
Ryan Walker <rwalker3801@gmail.com>
2016-06-17 12:33:33 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 document.getElementById("footer").innerHTML =
-"<p>&copy;  " + new Date().getFullYear() + " Ryan Walker</p>";
+	"<p>&copy;  " + new Date().getFullYear() + " Ryan Walker</p>";
+
 document.getElementById("nav01").innerHTML =
 "<ul id='menu'>" +
 	"<li><a href='index.html'>Home</a></li>" +
 	"<li><a href='about-me.html'>About Me</a></li>" +
 	"<li><a href='favorite-links.html'>Favorite Links</a></li>" +
+	"<li><a href='dank-memes.html'>Dank Memes</a></li>"
 "</ul>";
